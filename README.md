fd28361bad6280f0e3f80a0385a81a247b90c259
Ilya <Dragoilya@yandex.ru>
2023-03-06 06:04:51 +0300

    Done work

--- README.md
+++ README.md
@@ -2,11 +2,22 @@
 
 Сайт магазина авторского вина "Новое русское вино".
 
+## Требования к использованию
+
+Требуется [Python](https://www.python.org/downloads/) версии 3.7 или выше и установленный [pip](https://pip.pypa.io/en/stable/getting-started/). Для установки необходимых зависимостей используйте команду:  
+1. Для Unix/macOs:
+```commandline
+python -m pip install -r requirements.txt
+```
+2. Для Windows:
+```commandline
+py -m pip download --destination-directory DIR -r requirements.txt
+```
+
 ## Запуск
 
-- Скачайте код
-- Запустите сайт командой `python3 main.py`
-- Перейдите на сайт по адресу [http://127.0.0.1:8000](http://127.0.0.1:8000).
+1. Запустите сайт командой `python3 main.py`.
+2. Перейдите на сайт по адресу [http://127.0.0.1:8000](http://127.0.0.1:8000).
 
 ## Цели проекта
 
--- main.py
+++ main.py
@@ -1,5 +1,67 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from datetime import datetime
+import pprint
+from collections import defaultdict
+
+import pandas
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+def years_end(year: int) -> str:
+    year_double_end = year%100
+    if 11<=year_double_end<=20:
+        return "лет"
+    year_end = year%10
+    if year_end == 1:
+        return "год"
+    elif 1<year_end<5:
+        return "года"
+    else:
+        return "лет"
+
+
+def jinja2_initialize(years_together, wine_categories):
+    env = Environment(
+        loader=FileSystemLoader('.'),
+        autoescape=select_autoescape(['html', 'xml'])
+    )
+
+    template = env.get_template('template.html')
+
+    rendered_page = template.render(
+        wine_categories=wine_categories,
+        years_together=years_together,
+        years_together_end=years_end(years_together),
+    )
+
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
+
+    server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
+    server.serve_forever()
+
+
+def get_wines_sotrs_list_from_xlsx(xlsx_file_name, sheet_name="Лист1"):
+    wine_categories = defaultdict(list)
+    excel_data_df = pandas.read_excel(
+        io=xlsx_file_name,
+        sheet_name=sheet_name,
+        na_values=['N/A', 'NA'],
+        keep_default_na=False,
+    )
+    for data_row_dict in excel_data_df.to_dict(orient='record'):
+        wine_categories[data_row_dict["Категория"]].append(data_row_dict)
+
+    return wine_categories
+
+
+def main():
+    wine_categories = get_wines_sotrs_list_from_xlsx("wine3.xlsx")
+    pp = pprint.PrettyPrinter()
+    pp.pprint(wine_categories)
+    years_together = datetime.now().year - 1920
+    jinja2_initialize(years_together, wine_categories)
+
+
+if __name__ == '__main__':
+    main()
